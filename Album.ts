447289c1c18c014c0442269af59017f607a70b4f
ashrafmakandar <37826738+ashrafmakandar@users.noreply.github.com>
2025-12-09 17:52:24 +0530

    created types for album

---
+++ Album.ts
@@ -0,0 +1,9 @@
+export type Album={
+    collectionName: string;
+    artistName: string;
+    artworkUrl100: string;
+    releaseDate: string;
+    primaryGenreName: string;
+    country: string;
+
+}
--- Home.tsx
+++ Home.tsx
@@ -1,15 +1,20 @@
+/* eslint-disable react-native/no-inline-styles */
 import { Button, Text } from "@react-navigation/elements";
 import { useNavigation } from "@react-navigation/native";
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 
-export default function Home() {
+const Home =()=> {
    const navigation = useNavigation();
+   const []
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1}}>
           <Text>Home Screen</Text>
-      <Button onPress={() => navigation.navigate('Details')}>
-        Go to Details n
-      </Button>
+          <FlatList
+          data={}
+          />
+     
     </View>
   );
 }
+
+export default Home;
